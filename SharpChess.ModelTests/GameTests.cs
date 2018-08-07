88f88762fa4175ecaab84c8032e1c1f7ff5c142f
Brad Gustafson <greeninja13@gmail.com>
2018-08-06 23:22:14 -0600

    5 tests. going to sleep now.

--- SharpChess.ModelTests/GameTests.cs
+++ SharpChess.ModelTests/GameTests.cs
@@ -76,5 +76,40 @@ namespace SharpChess.Model.Tests
             Assert.AreEqual(r1 < k, k < r2);
 
         }
+
+        //Bishops on opposite colors
+        [TestMethod()]
+        public void GenerateChess960FenTest_Bishop()
+        {
+            string side1 = Game.GenerateChess960Fen().Substring(0, 8);
+            int b1 = 0, b2 = 0;
+
+            for(; b1 < side1.Length; b1++)
+            {
+                if (side1[b1].Equals('b'))
+                {
+                    break;
+                }
+            }
+            for (; b2 < side1.Length; b2++)
+            {
+                if (b2 != b1)
+                {
+                    if (side1[b2].Equals('b'))
+                    {
+                        break;
+                    }
+                }
+            }
+            Assert.AreNotEqual(b1 % 2, b2 % 2);
+        }
+
+        [TestMethod()]
+        public void GameStartPositionTest_valid()
+        {
+            string side1 = Fen.GameStartPosition.Substring(0,8);
+
+            Assert.AreEqual(side1, "rnbqkbnr");
+        }
     }
 }
